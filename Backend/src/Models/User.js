78e3f77c6ec0b5c3710f9818ff5d7e7c68577c5a
Arthur Rodrigues <127230206+ArthurPRodrigues@users.noreply.github.com>
2024-11-06 10:56:55 -0300

    Add: changes in user model

--- Backend/src/Models/User.js
+++ Backend/src/Models/User.js
@@ -5,9 +5,11 @@ const ProfessorSchema = new mongoose.Schema({
     pessoa: { type: PessoaSchema, required: true },
     departamento: { type: String, required: true },
     especialidade: { type: String },
-    unidades: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Unidade' }] // Array de referências a Unidades que o professor é responsável
-}, {
+    unidades: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Unidade' }],// Array de referências a Unidades que o professor é responsável
+    tipo_egresso: { type: String, required: true },
+    }, 
+{
     timestamps: true
 });
 
-module.exports = mongoose.model('Professor', ProfessorSchema);
+module.exports = mongoose.model('User', UserSchema);
