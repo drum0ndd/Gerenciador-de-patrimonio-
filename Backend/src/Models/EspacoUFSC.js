adf49604ab092d3708919c8a78a7d3ca9a007512
Arthur Rodrigues <127230206+ArthurPRodrigues@users.noreply.github.com>
2024-11-13 09:17:18 -0300

    Att: MongoDB, EspacoUFSC

--- Backend/src/Models/EspacoUFSC.js
+++ Backend/src/Models/EspacoUFSC.js
@@ -4,5 +4,6 @@ const EspacoUFSCSchema = new mongoose.Schema({
     nome: { type: String, required: true },
     descricao: { type: String, required: false },
     responsavel: { type: UserSchema, required: true }, // Referência a um professor (user = 1) responsável pela unidade
-    patrimonio: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Patrimonio', required: false }] // Array de referências a patrimônios da unidade
+    patrimonio: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Patrimonio', required: false }],
+    lista_participantes: [{ Type: list}] // Array de referências a patrimônios da unidade
 });
--- Backend/src/database/db.js
+++ Backend/src/database/db.js
@@ -4,8 +4,7 @@ const connectdatabase = () => {
   console.log("Waiting to connect the Database");
 
   mongoose.connect(
-    "mongodb+srv://arthurprodrigues1:root321root123@gerenciadordepatrimonio.csroj.mongodb.net/",
-    { useNewURLParser: true, useUnifiedTopology: true }
+    "mongodb+srv://arthurprodrigues1:root321root123@gerenciadordepatrimonio.csroj.mongodb.net/"
     )
     .then(() => console.log("MongoDB Atlas is connected."))  
     .catch((error) => console.log(error))
