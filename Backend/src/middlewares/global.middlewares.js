be50276365f8bafde207ab0f21bd640654861aaa
Arthur Rodrigues <127230206+ArthurPRodrigues@users.noreply.github.com>
2024-11-18 22:45:52 -0300

    Att: middleware

--- Backend/src/middlewares/global.middlewares.js
+++ Backend/src/middlewares/global.middlewares.js
@@ -1,5 +1,8 @@
 import mongoose from "mongoose";
 import userService from "../services/user.service.js";
+import PatrimonioService from "../services/Patrimonio.Service.js";
+import EmprestimoService from "../Services/Emprestimo.service.js";
+import EspacoUFSC from "../services/EspacoUFSC.service.js";
 
 export const validId = (req, res, next) => {
     const id = req.params.id;
@@ -27,6 +30,97 @@ export const validUser = async (req, res, next) => {
     }
 };
 
+export const validPatrimonio = async (req, res, next) => {
+    try {const id = req.params.id;
+
+    const patrimonio = await PatrimonioService.findByIdService(id);
+
+    if (!patrimonio) {
+        return res.status(404).send({ message: "Patrimonio não encontrado!" });
+    }
+
+    req.id = id 
+    req.patrimonio = patrimonio;
+
+    next();} catch (err) {
+        res.status(500).send({ message: err.message });
+    }
+};
+
+export const validEmprestimo = async (req, res, next) => {
+    try {const id = req.params.id;
+
+    const emprestimo = await EmprestimoService.findByIdService(id);
+
+    if (!emprestimo) {
+        return res.status(404).send({ message: "Emprestimo não encontrado!" });
+    }
+
+    req.id = id 
+    req.emprestimo = emprestimo;
+
+    next();} catch (err) {
+        res.status(500).send({ message: err.message });
+    }
+};
+
+export const validEspacoUFSC = async (req, res, next) => {
+    try {const id = req.params.id;
+
+    const espacoUFSC = await EspacoUFSC.findByIdService(id);
+
+    if (!espacoUFSC) {
+        return res.status(404).send({ message: "EspacoUFSC não encontrado!" });
+    }
+
+    req.id = id 
+    req.espacoUFSC = espacoUFSC;
+
+    next();} catch (err) {
+        res.status(500).send({ message: err.message });
+    }
+};
+
+export const validProfessor = async (req, res, next) => {
+    try {
+        const ProfessorCorreto = await userService.findByIdService(id);
+        if (!ProfessorCorreto){
+            return res.status(400).json({ message: "Professor não encontrado"});
+        }
+
+        //Verifica se o professor é um professor  mesmo
+        if (ProfessorCorreto.tipo_egresso === 2){
+            return res.status(400).json({ message: "O usuário informado não é um professor"});
+        }
+        next();
+    } catch (err) {
+        res.status(500).send({ message: err.message });
+    }
+};
+
+export const validAluno = async (req, res, next) => {
+    try{
+        const AlunoCorreto = await userService.findByIdService(id);
+        if (!AlunoCorreto){
+            return res.status(400).json({ message: "Aluno não encontrado"});
+        }
+
+        //Verifica se o aluno é um aluno mesmo
+        if (AlunoCorreto.tipo_egresso !== 1){
+            return res.status(400).json({ message: "O usuário informado não é um aluno"});
+        }
+    } catch (err) {
+        res.status(500).send({ message: err.message });
+    }
+
+};
+
 export default {
-    validId, validUser,
+    validId, 
+    validUser,
+    validPatrimonio,
+    validEmprestimo,
+    validEspacoUFSC,
+    validProfessor,
+    validAluno,
 };
