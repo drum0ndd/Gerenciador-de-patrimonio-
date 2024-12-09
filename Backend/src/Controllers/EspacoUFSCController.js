c8ceb0876fdbe980d17dc6966a7adf701e07ec31
Arthur Rodrigues <127230206+ArthurPRodrigues@users.noreply.github.com>
2024-12-09 09:42:46 -0300

    Update EspacoUFSCController.js

--- Backend/src/Controllers/EspacoUFSCController.js
+++ Backend/src/Controllers/EspacoUFSCController.js
@@ -1,6 +1,6 @@
 import EspacoUFSCService from "../services/espacoUFSC.service.js";
 import { isProfessor } from "../middlewares/global.middlewares.js";
-import espacoUFSCService from "../services/EspacoUFSC.service.js";
+import EspacoUFSCService from "../services/EspacoUFSC.service.js";
 import userService from "../services/user.service.js";
 
 
